dfe4cadcf85bc5cb32dbdeeab1c602d28d68f836
“solittletime” <solittletime@users.noreply.github.com>
2018-02-02 16:56:33 -0500

    Added gulp-develop-server, handlebars and express dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,13 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    coffee = require('gulp-coffee'),
-    connect = require('gulp-connect'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+var gulp = require( 'gulp' ), server = require( 'gulp-develop-server' );
 
-var coffeeSources = ['scripts/hello.coffee'],
-    jsSources = ['scripts/*.js'],
-    sassSources = ['styles/*.scss'],
-    htmlSources = ['**/*.html'],
-    outputDir = 'assets';
-
-
-gulp.task('log', function() {
-  gutil.log('== My First Task ==')
+// run server
+gulp.task( 'server:start', function() {
+  server.listen( { path: './app.js' } );
 });
 
-gulp.task('copy', function() {
-  gulp.src('index.html')
-  .pipe(gulp.dest(outputDir))
+// restart server if app.js changed
+gulp.task( 'server:restart', function() {
+  gulp.watch( [ './app.js' ], server.restart );
 });
 
-gulp.task('sass', function() {
-  gulp.src(sassSources)
-  .pipe(sass({style: 'expanded'}))
-    .on('error', gutil.log)
-  .pipe(gulp.dest('assets'))
-  .pipe(connect.reload())
-});
-
-gulp.task('coffee', function() {
-  gulp.src(coffeeSources)
-  .pipe(coffee({bare: true})
-    .on('error', gutil.log))
-  .pipe(gulp.dest('scripts'))
-});
-
-gulp.task('js', function() {
-  gulp.src(jsSources)
-  .pipe(uglify())
-  .pipe(concat('script.js'))
-  .pipe(gulp.dest(outputDir))
-  .pipe(connect.reload())
-});
-
-gulp.task('watch', function() {
-  gulp.watch(coffeeSources, ['coffee']);
-  gulp.watch(jsSources, ['js']);
-  gulp.watch(sassSources, ['sass']);
-  gulp.watch(htmlSources, ['html']);
-});
-
-gulp.task('connect', function() {
-  connect.server({
-    root: '.',
-    livereload: true
-  })
-});
-
-gulp.task('html', function() {
-  gulp.src(htmlSources)
-  .pipe(connect.reload())
-});
-
-gulp.task('default', ['html', 'coffee', 'js', 'sass', 'connect', 'watch']);
+gulp.task('serve', ['server:start']);
