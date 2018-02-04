e42c78b0511e251afbb50921bfa631624b7719d1
solittletime <solittletime@users.noreply.github.com>
2018-02-04 01:20:41 -0500

    Removed configuration server

--- server/index.js
+++ server/index.js
@@ -1,30 +1,11 @@
-//import gulpTemplate from './templates/gulp';
-
-//const express = require('express')
-//const app = express()
-
-//app.get('/', (req, res) => {
-//  res.send(gulpTemplate({ title: 'Getting started with Gulp' }));
-//});
-
-//app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
 import Server from './Server';
-import Settings from './Settings';
 import minimist from 'minimist';
 
 const argv = minimist(process.argv, {
   'default': {
-    'config-server-port': 8889,
     'server-port': 8888
   }
 });
+
 const server = new Server(argv['server-port']);
-const settings = new Settings(argv['config-server-port'], argv['server-port']);
-
-settings.listen();
-server.setConnectionType('perfect');
-
-settings.on('connectionChange', ({type}) => {
-  server.setConnectionType(type);
-});
+server.listen();
